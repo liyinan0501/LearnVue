42dbffdec1190171080e66aaf21f4eb80b60ff3b
Yinan Li <gabriel.li5566@gmail.com>
2022-09-03 15:50:53 +0300

    update Text Interpolation

---
+++ src/01-Text Interpolation.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h1>{{ obj.name }}</h1>
+    <h1>{{ obj.age >= 18 ? 'Adult' : 'Under age' }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello vue',
+      obj: {
+        name: 'Tom',
+        age: 5,
+      },
+    }
+  },
+}
+</script>
+
+<style></style>
