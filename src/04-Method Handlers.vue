a578a920b23059b54b49c4751ae3c153a265330b
Yinan Li <gabriel.li5566@gmail.com>
2022-09-03 15:52:48 +0300

    update Method Handlers

---
+++ src/04-Method Handlers.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <a @click="one" href="http://www.google.com">Google</a>
+    <hr />
+    <a @click="two(10, $event)" href="http://www.amazon.com">Amazon</a>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 1. 时间触发，无传值，可以直接获取事件对象。
+    one(event) {
+      event.preventDefault()
+    },
+    // 2. 事件触发，传值，需要手动传入$event。
+    two(num, event) {
+      event.preventDefault()
+    },
+  },
+}
+</script>
+
+<style></style>
