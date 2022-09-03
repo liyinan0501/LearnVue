a0257db5a00868b4ac2f49f87719fe9c92090fbe
Yinan Li <gabriel.li5566@gmail.com>
2022-09-03 15:53:16 +0300

    update Key Modifiers

---
+++ src/06-Key Modifiers.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- 1. 绑定键盘按下事件 -->
+    <!-- 回车键 -->
+    <input type="text" @keydown.enter="enterFn" />
+    <hr />
+    <!-- 退出键 -->
+    <input type="text" @keydown.esc="escFn" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    enterFn() {
+      console.log('Enter Triggered')
+    },
+    escFn() {
+      console.log('Esc Triggered')
+    },
+  },
+}
+</script>
+
+<style></style>
