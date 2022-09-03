06828868a9313fe5800fd9195ed92b695d504025
Yinan Li <gabriel.li5566@gmail.com>
2022-09-03 17:53:02 +0300

    update Case-Text Reverse

---
+++ src/07-Case-Text Reverse.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+    <button @click="reverseFn">Reverse</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello, World',
+    }
+  },
+  methods: {
+    reverseFn() {
+      let arr = this.msg.split('')
+      this.msg = arr.reverse().join('')
+    },
+  },
+}
+</script>
+
+<style></style>
