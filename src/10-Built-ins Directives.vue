59294b1d6f1ce1f8588272de0c0b6f0eada42208
Yinan Li <gabriel.li5566@gmail.com>
2022-09-03 17:53:46 +0300

    update Built-ins Directives

---
+++ src/10-Built-ins Directives.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- <span>This is a SPAN tag.</span> -->
+    <p v-text="str"></p>
+    <!-- This is a SPAN tag. -->
+    <p v-html="str"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      str: '<span>This is a SPAN tag.</span>',
+    }
+  },
+}
+</script>
+
+<style></style>
