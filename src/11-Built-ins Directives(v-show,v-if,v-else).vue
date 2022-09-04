1de2eacfee183d82b0af69a0ca7f32d56a0cc241
Yinan Li <gabriel.li5566@gmail.com>
2022-09-05 00:59:51 +0300

    update 11-Built-ins Directives(v-show,v-if,v-else)

---
+++ src/11-Built-ins Directives(v-show,v-if,v-else).vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- 
+      v-show 或 v-if 控制隐藏
+      * v-show='vue变量'
+          用display:none来隐藏。(适合频繁切换使用)
+      * v-if='vue变量'
+          直接从DOM树上移除。
+     -->
+    <h1 v-show="isShow">v-show Box</h1>
+    <h1 v-if="isOk">v-if Box</h1>
+
+    <!-- v-if 和 v-else -->
+    <p v-if="age >= 18">Adult</p>
+    <p v-else>Minor</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: false,
+      isOk: false,
+      age: 20,
+    }
+  },
+}
+</script>
+
+<style></style>
