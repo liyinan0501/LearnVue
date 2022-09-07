e0348d0cf44757e6173e6fb28ab8d736e763cc9d
Yinan Li <gabriel.li5566@gmail.com>
2022-09-08 01:35:29 +0300

    Dynamic ClassName and Style using v-bind shorthand

---
+++ src/20-Dynamic ClassName and Style(v-bind).vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- 
+      :class="{类名: 布尔值}"
+      使用场景：vue变量控制标签是否应该有类名
+     -->
+    <p :class="{ redStr: bool }">Dynamic ClassName</p>
+    <!-- 
+      :style="{css属性名: 值}"
+      使用场景：
+     -->
+    <p :style="{ backgroundColor: bg }">Dynamic Style</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bool: true,
+      bg: 'red',
+    }
+  },
+}
+</script>
+
+<style>
+.redStr {
+  color: red;
+}
+</style>
