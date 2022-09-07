74199fce3158bc783c50d73d5ebce73d173a6ba7
Yinan Li <gabriel.li5566@gmail.com>
2022-09-08 01:33:59 +0300

    configure global filters

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
+import moment from 'moment'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// 方式1：全局过滤器 main.js
+// 任意的 .vue文件内可直接使用。
+// 定义:
+// app.config.globalProperties.$filters = { 过滤器名(要过滤的值) {return '过滤后结果'} }
+app.config.globalProperties.$filters = {
+  reverse(str) {
+    return str.split('').reverse().join('')
+  },
+  formatDate() {
+    return moment().format('YYYY-MM-DD')
+  },
+}
+app.mount('#app')
