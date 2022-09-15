0bef71ff836195a72c28bf59e0abce28b6c06c79
Yinan Li <gabriel.li5566@gmail.com>
2022-09-15 12:46:18 +0300

    update Child to Parent Communication

---
+++ src/40-Child to Parent Communication.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <!-- 
+      子传父
+      1. 父组件 @自定义事件名="父methods函数"
+     -->
+    <MyProduct
+      v-for="({ id, proname, proprice, info }, ind) in list"
+      :key="id"
+      :title="proname"
+      :price="proprice"
+      :intro="info"
+      :index="ind"
+      @subprice="subFn"
+    ></MyProduct>
+  </div>
+</template>
+
+<script>
+import MyProduct from './components/MyProductSub.vue'
+export default {
+  data() {
+    return {
+      list: [
+        {
+          id: 1,
+          proname: '超级好吃的棒棒糖',
+          proprice: 18.8,
+          info: '开业大酬宾, 全场8折',
+        },
+        {
+          id: 2,
+          proname: '超级好吃的大鸡腿',
+          proprice: 34.2,
+          info: '好吃不腻, 快来买啊',
+        },
+        {
+          id: 3,
+          proname: '超级无敌的冰激凌',
+          proprice: 14.2,
+          info: '炎热的夏天, 来个冰激凌了',
+        },
+      ],
+    }
+  },
+  components: {
+    MyProduct,
+  },
+  methods: {
+    subFn(index, price) {
+      this.list[index].proprice > 1 &&
+        (this.list[index].proprice = (
+          this.list[index].proprice - price
+        ).toFixed(2))
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/MyProductSub.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="my-product">
+    <h3>标题: {{ title }}</h3>
+    <p>价格: {{ price }}元</p>
+    <p>{{ intro }}</p>
+    <!-- <button @click="subFn">Cut -1</button> -->
+    <button @click="subFn">Cut -1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['index', 'title', 'price', 'intro'],
+  methods: {
+    // 错误：
+    // subFn() {
+    //   1. 子组件改父传入的数据不通知父亲 -> 数据不一致
+    //   2. vue规定props 本身是只读的，不允许重新赋值
+    //   this.price = this.price - 1
+    // },
+
+    // 2. this.$emit() - 主动触发事件
+    subFn() {
+      this.$emit('subprice', this.index, 1)
+    },
+  },
+}
+</script>
+
+<style>
+.my-product {
+  width: 400px;
+  padding: 20px;
+  border: 2px solid #000;
+  border-radius: 5px;
+  margin: 10px;
+}
+</style>
