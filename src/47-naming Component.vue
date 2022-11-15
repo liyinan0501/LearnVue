6d9e60ee40b637f14e3d74b1b72c3583a829b897
Yinan Li <gabriel.li5566@gmail.com>
2022-11-15 21:00:42 +0200

    naming an import component

---
+++ src/47-naming Component.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ComNameHaHa></ComNameHaHa>
+  </div>
+</template>
+
+<script>
+import Com from "./components/nameComponent.vue";
+export default {
+  components: {
+    [Com.name]: Com,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/nameComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>nameComponent</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComNameHaHa", //注册时定义自己的名字
+};
+</script>
+
+<style></style>
