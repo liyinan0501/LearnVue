b6a4605d7b185f8929d4077a9275e58973ac5b58
Yinan Li <gabriel.li5566@gmail.com>
2022-11-15 20:43:06 +0200

    update 44 $refs basic use

---
+++ src/44-$refs-retrive DOM and Component.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>1. Retrive DOM</p>
+    <h1 id="h" ref="myH">Header One</h1>
+
+    <p>2. Retrive Component</p>
+    <Demo ref="de"></Demo>
+  </div>
+</template>
+
+<script>
+import Demo from "./components/demoComponent.vue";
+export default {
+  mounted() {
+    console.log(document.getElementById("h"));
+    console.log(this.$refs.myH);
+    let demoObj = this.$refs.de;
+    demoObj.fn();
+  },
+  components: {
+    Demo,
+  },
+};
+</script>
+
+<style></style>
