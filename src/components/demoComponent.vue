552f17ef2b18dd09dcbbd249e282ed1f1fd47941
Yinan Li <gabriel.li5566@gmail.com>
2022-11-15 20:52:56 +0200

    retrive the method from an import component

---
+++ src/components/demoComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>DEMO component</p>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    fn() {
+      console.log("This from DEMO component's fn()");
+    },
+  },
+};
+</script>
+
+<style></style>
